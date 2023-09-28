63ae60e06eccdb5c7ef2ded532295682a23927d9
alexisgrstn <alexis.grastien@ynov.com>
2023-09-28 12:12:55 +0200

    Func Dead

--- personnage.go
+++ personnage.go
@@ -61,3 +61,16 @@ func (joueur *Personnage) SpellBook() {
 	joueur.Sort = append(joueur.Sort, "Boule de feu")
 }
 
+func (joueur *Personnage) dead() {
+	if joueur.PvActuels <= 0 {
+		clearScreen()
+		fmt.Println("=======================================")
+		fmt.Print("\033[31m") // Définir la couleur du texte en rouge
+		fmt.Println("           VOUS ÊTES MORT !           ")
+		fmt.Print("\033[0m") // Réinitialiser la couleur du texte
+		fmt.Println("=======================================")
+		joueur.PvActuels = joueur.PvMax / 2
+		fmt.Printf("vous avez été réanimé avec %d point de vie grâce à une RedBull.\n", joueur.PvActuels)
+		fmt.Println("Prenez garde et continuez votre aventure!")
+	}
+}
