c433d78af6a2a299fb7baf9fa9f275ec6ceafeef
alexisgrstn <alexis.grastien@ynov.com>
2023-09-28 12:06:57 +0200

    afficher menu

--- menu.go
+++ menu.go
@@ -11,3 +11,16 @@ func handleMenu(joueur *Personnage) {
 	executeChoice(joueur, choix)
 }
 
+func afficherMenu() {
+	fmt.Println("Menu:")
+	fmt.Println("=======================================")
+	fmt.Println("1. Afficher les informations du personnage")
+	fmt.Println("2. Accéder à l'inventaire du personnage")
+	fmt.Println("3. Marchand")
+	fmt.Println("4. Quitter")
+	fmt.Println("=======================================")
+	fmt.Print("Choisissez une option (1, 2, 3 ou 4): ")
+	fmt.Println('\n')
+	fmt.Println("=======================================")
+}
+
