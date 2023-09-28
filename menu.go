7128033e39fb485aa3bcac4f624e68c5879607e2
alexisgrstn <alexis.grastien@ynov.com>
2023-09-28 12:08:30 +0200

    executer le choix

--- menu.go
+++ menu.go
@@ -33,3 +33,18 @@ func getChoice() int {
 	return choix
 }
 
+func executeChoice(joueur *Personnage, choix int) {
+	switch choix {
+	case 1:
+		joueur.displayInfo()
+	case 2:
+		menuInventaire(joueur)
+	case 3:
+		marchand(joueur)
+	case 4:
+		fmt.Println("Au revoir !")
+		os.Exit(0)
+	default:
+		fmt.Println("Option invalide. Veuillez entrer 1, 2, 3 ou 4.")
+	}
+}
