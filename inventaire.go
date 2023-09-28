6fdc51a0f782e00268a0f26f168a766881277f3d
alexisgrstn <alexis.grastien@ynov.com>
2023-09-28 11:58:13 +0200

    Menu Inventaire

--- inventaire.go
+++ inventaire.go
@@ -8,3 +8,22 @@ import (
 type Marchand struct {
 	ItemsEnVente []string
 }
+func menuInventaire(joueur *Personnage) {
+	for {
+		clearScreen()
+		fmt.Println("=======================================")
+		fmt.Println("Inventaire du personnage:")
+		fmt.Println("=======================================")
+		for i, item := range joueur.Inventaire {
+			fmt.Printf("%d. %s (%d)\n", i+1, item.Name, item.Value)
+		}
+		fmt.Println("=======================================")
+		fmt.Println("0. Retour au menu précédent")
+		inventoryChoice := AskForInventoryChoice(joueur)
+		if inventoryChoice == 0 {
+			break
+		}
+		useInventoryItem(joueur, inventoryChoice)
+	}
+}
+
